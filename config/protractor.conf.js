e8827525913990e04a95e51b40c95f8c7bc57e58
Jason Stone <rolaveric@gmail.com>
2014-05-16 21:04:06 +1000

    Removed drag n' drop example.  Added `npm start` script.

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -2,8 +2,7 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: ['../test/e2e/*.js'],
 	baseUrl: 'http://localhost:8000/',
-	multiCapabilities: [
-		{'browserName': 'chrome'},
-		{'browserName': 'firefox'}
-	]
+	capabilities: {
+		'browserName': 'chrome'
+	}
 };
